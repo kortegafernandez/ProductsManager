ef24c91b2971f7f6ee253c06e4a9693037779ea1
Karol Ortega <kortegafernandez@gmail.com>
2024-01-31 21:51:08 -0500

    Refactor endpoints mapping configuration

---
+++ ProductsManager/ProductsManager.API/Modules/ProductsModule.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.OpenApi.Models;
+using ProductsManager.Application.Products.Commands.Create;
+using ProductsManager.Application.Products.Commands.Update;
+using ProductsManager.Application.Products.Queries.GetAll;
+using ProductsManager.Application.Products.Queries.GetById;
+
+namespace ProductsManager.API.Modules
+{
+    public static class ProductsModule
+    {
+        public static void AddProductsEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductCommand command, IMediator mediator) =>
+            {
+                await mediator.Send(command);
+                Results.Created();
+            })
+            .WithName("CreateProduct")
+            .WithOpenApi(x => new OpenApiOperation(x)
+            {
+                Summary = "Add a new product",
+                Description = "Adds a product with its current price.",
+            });
+
+            app.MapGet("/products", async (IMediator mediator) =>
+            {
+                var response = await mediator.Send(new GetProductsQuery());
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .WithOpenApi();
+
+            app.MapGet("/products/{id}", async (int id, IMediator mediator) =>
+            {
+                var response = await mediator.Send(new GetProductByIdQuery() { Id = id });
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .WithOpenApi();
+
+            app.MapPut("/products", async (UpdateProductCommand command, IMediator mediator) =>
+            {
+                await mediator.Send(command);
+                Results.Ok();
+            })
+            .WithName("UpdateProduct")
+            .WithOpenApi();
+        }
+    }
+}
--- ProductsManager/ProductsManager.API/Program.cs
+++ ProductsManager/ProductsManager.API/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.OpenApi.Models;
 using NLog.Web;
 using ProductsManager.API.Middlewares;
+using ProductsManager.API.Modules;
 using ProductsManager.Application;
 using ProductsManager.Application.Products.Commands.Create;
 using ProductsManager.Application.Products.Commands.Update;
@@ -44,41 +45,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("/products", async (CreateProductCommand command, IMediator mediator) =>
-{
-    await mediator.Send(command);
-    Results.Created();
-})
-.WithName("CreateProduct")
-.WithOpenApi(x => new OpenApiOperation(x)
- {
-     Summary = "Add a new product",
-     Description = "Adds a product with its current price.",
- });
-
-app.MapGet("/products", async (IMediator mediator) =>
-{
-    var response = await mediator.Send(new GetProductsQuery());
-    return Results.Ok(response);
-})
-.WithName("GetProducts")
-.WithOpenApi();
-
-app.MapGet("/products/{id}", async (int id,IMediator mediator) =>
-{
-    var response = await mediator.Send(new GetProductByIdQuery() { Id = id});
-    return Results.Ok(response);
-})
-.WithName("GetProductById")
-.WithOpenApi();
-
-app.MapPut("/products", async (UpdateProductCommand command, IMediator mediator) =>
-{
-    await mediator.Send(command);
-    Results.Ok();
-})
-.WithName("UpdateProduct")
-.WithOpenApi();
+app.AddProductsEndpoints();
 
 app.UseMiddleware<ResponseWrapperMiddleware>();
 
